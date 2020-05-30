f1f112aff340abd045d09360462a690ec9fd01ce
Lance Albertson <lance@osuosl.org>
2020-05-29 21:37:51 -0700

    Updates for Train
    Changed:
    -Update release to train
    
    Removed:
    - Unused rubocop rules
    
    Change-Id: Ibd5949820b50e05c07b39d893547416e0096193c
    Depends-On: https://review.opendev.org/731849
    Depends-On: https://review.opendev.org/731861

--- metadata.rb
+++ metadata.rb
@@ -7,13 +7,13 @@ source_url        'https://opendev.org/openstack/cookbook-openstack-dns'
 license           'Apache-2.0'
 description       'Installs and configures the Designate Service'
 chef_version      '>= 15.0'
-version           '19.0.0'
+version           '20.0.0'
 
 %w(ubuntu redhat centos).each do |os|
   supports os
 end
 
 depends 'openstackclient'
-depends 'openstack-common', '>= 19.0.0'
-depends 'openstack-identity', '>= 19.0.0'
-depends 'openstack-network', '>= 19.0.0'
+depends 'openstack-common', '>= 20.0.0'
+depends 'openstack-identity', '>= 20.0.0'
+depends 'openstack-network', '>= 20.0.0'
