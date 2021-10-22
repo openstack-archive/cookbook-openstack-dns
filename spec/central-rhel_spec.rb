c6042984e7405112256ac744d11400a928bcbb76
Lance Albertson <lance@osuosl.org>
2021-10-22 16:05:34 -0700

    CentOS 8 support
    - Update ChefSpec
    
    Depends-On: https://review.opendev.org/c/openstack/cookbook-openstack-network/+/815172
    Change-Id: I617cdb3879dbb1827dff542888808f96dd988c1e
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- spec/central-rhel_spec.rb
+++ spec/central-rhel_spec.rb
@@ -1,28 +1,30 @@
 require_relative 'spec_helper'
 
 describe 'openstack-dns::central' do
-  describe 'redhat' do
-    let(:runner) { ChefSpec::SoloRunner.new(REDHAT_OPTS) }
-    let(:node) { runner.node }
-    cached(:chef_run) do
-      runner.converge(described_recipe)
-    end
+  ALL_RHEL.each do |p|
+    context "redhat #{p[:version]}" do
+      let(:runner) { ChefSpec::SoloRunner.new(p) }
+      let(:node) { runner.node }
+      cached(:chef_run) do
+        runner.converge(described_recipe)
+      end
 
-    include_context 'dns-stubs'
+      include_context 'dns-stubs'
 
-    it 'converges successfully' do
-      expect { :chef_run }.to_not raise_error
-    end
-    it do
-      expect(chef_run).to enable_service('designate_central').with(
-        service_name: 'designate-central',
-        supports: { restart: true, status: true }
-      )
-      expect(chef_run).to start_service('designate_central')
-    end
-    it do
-      expect(chef_run.service('designate_central')).to \
-        subscribe_to('template[/etc/designate/designate.conf]').on(:restart)
+      it 'converges successfully' do
+        expect { :chef_run }.to_not raise_error
+      end
+      it do
+        expect(chef_run).to enable_service('designate_central').with(
+          service_name: 'designate-central',
+          supports: { restart: true, status: true }
+        )
+        expect(chef_run).to start_service('designate_central')
+      end
+      it do
+        expect(chef_run.service('designate_central')).to \
+          subscribe_to('template[/etc/designate/designate.conf]').on(:restart)
+      end
     end
   end
 end
--- spec/mdns-rhel_spec.rb
+++ spec/mdns-rhel_spec.rb
@@ -1,28 +1,30 @@
 require_relative 'spec_helper'
 
 describe 'openstack-dns::mdns' do
-  describe 'redhat' do
-    let(:runner) { ChefSpec::SoloRunner.new(REDHAT_OPTS) }
-    let(:node) { runner.node }
-    cached(:chef_run) do
-      runner.converge(described_recipe)
-    end
+  ALL_RHEL.each do |p|
+    context "redhat #{p[:version]}" do
+      let(:runner) { ChefSpec::SoloRunner.new(p) }
+      let(:node) { runner.node }
+      cached(:chef_run) do
+        runner.converge(described_recipe)
+      end
 
-    include_context 'dns-stubs'
+      include_context 'dns-stubs'
 
-    it 'converges successfully' do
-      expect { :chef_run }.to_not raise_error
-    end
-    it do
-      expect(chef_run).to enable_service('designate_mdns').with(
-        service_name: 'designate-mdns',
-        supports: { restart: true, status: true }
-      )
-      expect(chef_run).to start_service('designate_mdns')
-    end
-    it do
-      expect(chef_run.service('designate_mdns')).to \
-        subscribe_to('template[/etc/designate/designate.conf]').on(:restart)
+      it 'converges successfully' do
+        expect { :chef_run }.to_not raise_error
+      end
+      it do
+        expect(chef_run).to enable_service('designate_mdns').with(
+          service_name: 'designate-mdns',
+          supports: { restart: true, status: true }
+        )
+        expect(chef_run).to start_service('designate_mdns')
+      end
+      it do
+        expect(chef_run.service('designate_mdns')).to \
+          subscribe_to('template[/etc/designate/designate.conf]').on(:restart)
+      end
     end
   end
 end
